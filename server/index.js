32c12a8b8b21a35e5019a63dac7e802db9b45060
kobemontes571-bit <kobemontes571@gmail.com>
2026-07-04 10:17:58 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-
-app.use(express.json());
 const path = require("path");
 
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client")));
 
 // ======================
